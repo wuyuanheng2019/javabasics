985b89f3942066f2b7d7784974fb7aac43a2f9ae
wyh <16619925116@163.com>
2019-05-06 10:48:15 +0800

    一致性hash算法

--- src/main/java/com/example/javabasics/hashagreement/code/ConsistenceHash.java
+++ src/main/java/com/example/javabasics/hashagreement/code/ConsistenceHash.java
@@ -6,17 +6,32 @@ import java.util.*;
 
 public class ConsistenceHash {
 
-    //物理节点集合
+    /**
+     * 物理节点集合
+     */
     private List<String> realNodes = new ArrayList<>();
 
-    //物理节点与虚拟节点的对应关系，存储的是虚拟节点的hash值
+    /**
+     * 物理节点与虚拟节点的对应关系，存储的是虚拟节点的hash值
+     */
     private Map<String, List<Integer>> real2VirtualMap = new HashMap<>();
 
+    /**
+     * 虚拟节点的个数
+     */
     private int virutailNums = 100;
 
-    //排序存储结构 红黑树，key是虚拟节点的hash值，value是物理节点
+    /**
+     * 排序存储结构 红黑树，key是虚拟节点的hash值，value是物理节点（方便找到数据关系：虚拟节点---物理节点）
+     * 所以使用了树结构
+     */
     private SortedMap<Integer, String> sortedMap = new TreeMap<>();
 
+    /**
+     * 指定创建虚拟节点个数
+     *
+     * @param virutailNums 虚拟节点
+     */
     public ConsistenceHash(int virutailNums) {
         super();
         this.virutailNums = virutailNums;
@@ -25,11 +40,17 @@ public class ConsistenceHash {
     public ConsistenceHash() {
     }
 
-    //加入服务器的方法
+    /**
+     * 加入服务器的方法
+     *
+     * @param node 服务器节点
+     */
     public void addServer(String node) {
+        //加入物理节点
         this.realNodes.add(node);
-        String vnode = null;
         int i = 0, count = 0;
+        //创建虚拟节点，并建立物理节点和虚拟节点的关系
+        String vnode = null;
         List<Integer> virtualNodes = new ArrayList<>();
         this.real2VirtualMap.put(node, virtualNodes);
 
@@ -37,8 +58,11 @@ public class ConsistenceHash {
         while (count < this.virutailNums) {
             i++;
             vnode = node + "&&" + i;
+            //得到对应的hash值（自定义的hash算法，为了得到唯一）
             int hashValue = FNV1_32_HASH.getHash(vnode);
+            //判断map中是否包含key
             if (!this.sortedMap.containsKey(hashValue)) {
+                //虚拟节点放入map
                 virtualNodes.add(hashValue);
                 this.sortedMap.put(hashValue, node);
                 count++;
@@ -69,16 +93,19 @@ public class ConsistenceHash {
             Integer vhash = subMap.firstKey();
             return subMap.get(vhash);
         } else {
+            //返回物理节点的位置
             return sortedMap.get(sortedMap.firstKey());
         }
     }
 
     public static void main(String[] args) {
         ConsistenceHash ch = new ConsistenceHash();
+        //放入服务器（也可以是缓存数据库的地址），缓存做集群
         ch.addServer("192.168.1.10");
         ch.addServer("192.168.1.11");
         ch.addServer("192.168.1.12");
 
+        //只是模拟了取出数据的规则，在应用时，需数据放入节点的方法
         for (int i = 0; i < 10; i++) {
             System.out.println("a" + i + "对应的服务器：" + ch.getServer("a" + i));
         }
